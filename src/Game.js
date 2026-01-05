8fe78a0f51cc41f1bd57786e87a640b6470f41df
droritos <drorgu102@gmail.com>
2026-01-05 16:14:00 +0200

    update time

--- src/Game.js
+++ src/Game.js
@@ -32,8 +32,8 @@ export default class Game {
         this.particles = [];
         this.texts = [];
         this.gold = 0; // Persistent Gold
-        this.energy = 100;
-        this.maxEnergy = 100;
+        this.energy = 999;
+        this.maxEnergy = 999;
         this.shake = 0;
 
         // Persistent Upgrades
